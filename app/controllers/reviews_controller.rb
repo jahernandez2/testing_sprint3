3e17d67f468cdc3a2503a7dc370a432302e46aa5
Franco Limon <francoenrique@live.com>
2022-10-18 19:15:22 -0500

    update

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,19 +1,11 @@
 class ReviewsController < ApplicationController
     def new
-        @hello = params[:anchor]
         @review = Review.new
-        session[:publication_id] = params[:publication_id]
     end
 
     def create
-        @review = Review.new(reviews_params)
-        @review.publication_id = session[:publication_id]
-        if @review.save
-          redirect_to root_path
-        else
-          render :new
-        end
-        session[:publication_id] = nil
+        @publication = Publication.new
+        @review = Publication.new
     end
 
     def reviews_params
--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,11 @@ Rails.application.routes.draw do
   devise_for :users
   resources :pivots
   resources :messages
-  resources :publications
+  resources :publications do
+    resources :reviews
+  end
   post 'pivots/new', to: 'pivots#create', as: 'create_pivot'
-  resources :reviews
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # Create reviews
 
