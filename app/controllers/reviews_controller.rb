4d1d1f9b5c28bd0978c1b06e470c71a39b8f1c98
Franco Limon <francoenrique@live.com>
2022-10-18 22:40:33 -0500

    commit

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,14 +1,20 @@
 class ReviewsController < ApplicationController
+
     def new
-        @review = Review.new
+        @publication = Publication.find(params[:publication_id])
+        @review = @publication.reviews.build()
     end
 
     def create
-        @publication = Publication.new
-        @review = Publication.new
+        @publication = Publication.find(params[:publication_id])
+        @review = @publication.reviews.build(reviews_params)
+        if @review.save
+            redirect_to root_path
+        end
     end
 
     def reviews_params
         params.require(:review).permit(:publication_id, :review)
     end
+    
 end
